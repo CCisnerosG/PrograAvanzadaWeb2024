3e11bb70bed25c44c9a570486a5d54b2bc08fc76
Carlos Cisneros <116586023+CCisnerosG@users.noreply.github.com>
2024-06-04 18:33:02 -0600

    1st commit

---
+++ PrograAvanzada/Northwind202402/BackEnd/Controllers/CategoryController.cs
@@ -0,0 +1,63 @@
+﻿using BackEnd.Services.Interfaces;
+using Entities.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+
+        private ICategoryService _categoryService;
+
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            this._categoryService = categoryService;
+        }
+
+        // GET: api/<CategoryController>
+        [HttpGet]
+        public IEnumerable<Category> Get()
+        {
+            return _categoryService.Get();
+        }
+
+        // GET api/<CategoryController>/5
+        [HttpGet("{id}")]
+        public Category Get(int id)
+        {
+            return _categoryService.Get(id);
+        }
+
+        // POST api/<CategoryController>
+        [HttpPost]
+        public Category Post([FromBody] Category category)
+        {
+            _categoryService.Add(category);
+            return category;
+
+        }
+
+        // PUT api/<CategoryController>/5
+        [HttpPut]
+        public Category Put([FromBody] Category category)
+        {
+            _categoryService.Update(category);
+            return category;
+        }
+
+        // DELETE api/<CategoryController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Category category = new Category { CategoryId = id };
+            _categoryService.Remove(category);
+
+        }
+    }
+}
---
+++ PrograAvanzada/Northwind202402/BackEnd/Services/Implementations/CategoryService.cs
@@ -0,0 +1,45 @@
+﻿using BackEnd.Services.Interfaces;
+using DAL.Interfaces;
+using Entities.Entities;
+
+namespace BackEnd.Services.Implementations
+{
+    public class CategoryService : ICategoryService
+    {
+
+        private IUnidadDeTrabajo _unidadDeTrabajo;
+
+        private ICategoryDAL categoryDAL;
+
+        public CategoryService(IUnidadDeTrabajo unidadDeTrabajo)
+        {
+                this._unidadDeTrabajo = unidadDeTrabajo;
+        }
+
+
+        public bool Add(Category category)
+        {
+           return _unidadDeTrabajo.CategoryDAL.Add(category);
+        }
+
+        public Category Get(int id)
+        {
+            return _unidadDeTrabajo.CategoryDAL.Get(id);
+        }
+
+        public IEnumerable<Category> Get()
+        {
+            return _unidadDeTrabajo.CategoryDAL.GetAll();
+        }
+
+        public bool Remove(Category category)
+        {
+            return _unidadDeTrabajo.CategoryDAL.Remove(category);
+        }
+
+        public bool Update(Category category)
+        {
+            return _unidadDeTrabajo.CategoryDAL.Update(category);    
+        }
+    }
+}
